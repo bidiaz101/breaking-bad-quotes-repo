71f071cfc2db52859f9bcacaa2032bd460c2e154
Brian Diaz <bidiaz101@gmail.com>
2021-09-29 00:39:37 -0500

    Fixed quote page dropdown options

--- breaking-bad-quotes/src/components/Quotes.js
+++ breaking-bad-quotes/src/components/Quotes.js
@@ -2,10 +2,13 @@ import React, { useState } from "react"
 import CardContainer from "./CardContainer"
 
 function Quotes({characterData, quoteData}) {
-    const [ selectedAuthor, setAuthor ] = useState("All")
-    console.log(quoteData)
+    const [ selectedAuthor, setAuthor ] = useState("All") 
 
-    const authorOptions = quoteData.filter(quoteObj => quoteObj.series === "Breaking Bad").map(quoteObj => <option value={quoteObj.author} key={quoteObj.quote_id}>{quoteObj.author}</option>)
+    const authorArr = ["All", "Walter White", "Jesse Pinkman", "Skyler White", "Saul Goodman", "Hank Schrader", "Mike Ehrmantraut", "Gus Fring", "Hector Salamanca"]
+
+    const authorOptions = authorArr.map(author => {
+        return <option value={author}key={author}>{author}</option>
+    })
 
     return (
         <div>
@@ -14,7 +17,6 @@ function Quotes({characterData, quoteData}) {
                 <label>Filter by Author:</label>
                 <br />
                 <select onChange={(e) => setAuthor(e.target.value)}>
-                    <option value="All">All</option>
                     {authorOptions}
                 </select>
             </form>
