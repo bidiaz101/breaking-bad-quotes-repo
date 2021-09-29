ea6cf45b60a6d6c86b3cce352c6f2de66ffd6cee
Brian Diaz <bidiaz101@gmail.com>
2021-09-29 02:19:09 -0500

    added pictures to accordion

--- breaking-bad-quotes/src/components/Deaths.js
+++ breaking-bad-quotes/src/components/Deaths.js
@@ -10,19 +10,19 @@ function Deaths({ characterData }) {
         .then(data => setDeathData(data))
     }, [])
 
-    console.log(deathData)
-
     const accordionOfDeath = deathData.sort((death1, death2) => death1.episode - death2.episode).sort((death1, death2) => death1.season - death2.season).map(death => {
+        const character = characterData.find(character => character.name === death.death)
+
         return (
             <Accordion.Item key={death.death_id} eventKey={death.death_id}>
                 <Accordion.Header>{death.death}</Accordion.Header>
                 <Accordion.Body>
+                    {character ? <img src={character.img} alt={character.name} /> : null}
                     <p>Occured in: Season {death.season}, Episode {death.episode}</p>
                     <p>Cause of Death: {death.cause}</p>
                     <p>Last Words: {death.last_words}</p>
                     {death.number_of_deaths > 1 ? <p>Number of Deaths: {death.number_of_deaths}</p> : null}
                     <p>Responsible: {death.responsible}</p>
-
                 </Accordion.Body>
             </Accordion.Item>
         )
